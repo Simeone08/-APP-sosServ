1bf1bfed61a2c73580da380035a0ba4472e17355
Werles Simeone <simeone878@gmail.com>
2024-06-22 13:01:42 -0300

    initial commit

---
+++ src/screens/form/form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './formStyles';
+
+export function form() {
+  return (
+    <View style={styles.container}>
+        <Text>FORM</Text>
+    </View>
+  );
+}
